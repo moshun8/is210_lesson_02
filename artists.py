8b4b46e718fbc9ea3239dc6347cae41a9a6396a1
Maureen Kilmade <maureen.kilmade@spsmail.cuny.edu>
2014-09-05 13:38:40 -0400

    Did Tasks 4-7

--- artists.py
+++ artists.py
@@ -3,5 +3,13 @@
 """Asks the great question."""
 
 
-THE_GREAT_QUESTION = ('Michaelangelo. Leonardo. Rafael. Donatello. Turtles? '
+the_great_question = ('Michaelangelo. Leonardo. Rafael. Donatello. Turtles? '
                       'Creators of the great works? Both? You be the judge!')
+statements = the_great_question.split('. ')
+artists = statements[0:4]
+
+num_artists = len(artists) #4
+characters = len(the_great_question) #105
+
+has_creators = 'Creator' in the_great_question #returns true
+has_splinter = 'Splinter' in artists #returns false
