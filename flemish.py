7658c3a8b0bb168701a630dc713b3ecf64038efd
Maureen Kilmade <maureen.kilmade@spsmail.cuny.edu>
2014-09-06 11:25:32 -0400

    Who knew there was a Flemish Inquisition

--- flemish.py
+++ flemish.py
@@ -2,10 +2,8 @@
 # -*- coding: utf-8 -*-
 """Contains expectations."""
 
-
 import inquisition
 fishy = inquisition.SPANISH.replace('surprise', 'haddock')
 find_spanish = fishy.index("Spanish")
 len_of_spanish = len("Spanish")
-after_spanish = fishy[(find_spanish+len_of_spanish):-1]
-flemish = fishy[0:find_spanish] + "Flemish" + after_spanish
+flemish = fishy[0:find_spanish] + "Flemish" + fishy[(find_spanish+len_of_spanish):-1]
