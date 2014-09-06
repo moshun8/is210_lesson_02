f817a3dab8d0a2ca702bf01707717d4686e0b7bb
Maureen Kilmade <maureen.kilmade@spsmail.cuny.edu>
2014-09-06 13:08:41 -0400

    000042

--- formatted.py
+++ formatted.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """Provides variables for formatting."""
 
-NEWS = 'Hi {0}! I have {1} news! I won the raffle with number {2:06.4f}!'
+NEWS = 'Hi {0}! I have {1} news! I won the raffle with number {2:06.0f}!'
 FNAME = 'Pat'
 NTYPE = '*amazing*'
 RNUM = 42
