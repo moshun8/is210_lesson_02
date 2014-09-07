1f847f49713cae8a49c57ef0fc40266c5fe802c1
Maureen Kilmade <maureen.kilmade@spsmail.cuny.edu>
2014-09-06 20:20:51 -0400

    Mixed formatting

--- formatted.py
+++ formatted.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 """Provides variables for formatting."""
 
-NEWS = 'Hi {0}! I have {1} news! I won the raffle with number {2:06.0f}!'
+NEWS = 'Hi {friend}! I have {0} news! I won the raffle with number {1:06.0f}!'
 FNAME = 'Pat'
 NTYPE = '*amazing*'
 RNUM = 42
 
-EMAIL = NEWS.format(FNAME, NTYPE, RNUM)
+EMAIL = NEWS.format(NTYPE, RNUM, friend = FNAME)
