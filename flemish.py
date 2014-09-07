233008bf50fabfd94ccb397916b886008a6ea05a
Maureen Kilmade <maureen.kilmade@spsmail.cuny.edu>
2014-09-06 20:52:37 -0400

    Flemish inquisition

--- flemish.py
+++ flemish.py
@@ -6,5 +6,5 @@ import inquisition
 FISHY = inquisition.SPANISH.replace('surprise', 'haddock')
 FIND_SPANISH = FISHY.index("Spanish")
 LEN_OF_SPANISH = len("Spanish")
-FLEMISH = (FISHY[0:FIND_SPANISH] + "Flemish" + 
-    FISHY[(FIND_SPANISH+LEN_OF_SPANISH)::])
+FLEMISH=
+    (FISHY[0:FIND_SPANISH] + "Flemish" + FISHY[(FIND_SPANISH+LEN_OF_SPANISH)::])
